25c7fc1c70896f452d3d643ff29a12bd317a0e2a
Higor W. Nunes <higorn@gmail.com>
2017-06-19 12:24:37 -0300

    login control

--- curso4/esseEuJaLi/frontend/EsseEuJaLi-UI/src/app/app.component.ts
+++ curso4/esseEuJaLi/frontend/EsseEuJaLi-UI/src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
 
   constructor(private router: Router) {
 
--- curso4/esseEuJaLi/frontend/EsseEuJaLi-UI/src/app/login/login.component.spec.ts
+++ curso4/esseEuJaLi/frontend/EsseEuJaLi-UI/src/app/login/login.component.spec.ts
@@ -1,25 +1,53 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {CustomMdModule} from '../custom-md/custom-md.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Router} from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let router: Router;
+
+  class MockRouter {
+      navigate(commands: any[]): Promise<boolean> {
+        return Promise.resolve(true);
+      }
+  }
+  const mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [
+          BrowserAnimationsModule,
+          CustomMdModule,
+      ],
+      providers: [
+        Location,
+        { provide: Router, useClass: MockRouter },
+      ]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(inject([Router], (_router: Router) => {
+      router = _router;
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should login', () => {
+      const spy = spyOn(router, 'navigate');
+    component.onLogin();
+    expect(router.navigate).toHaveBeenCalledWith(['/home']);
   });
 });
